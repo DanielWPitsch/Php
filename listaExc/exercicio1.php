2c3cc4a97d22675989aa00501ea0181ff13cf2da
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2022-09-27 23:44:14 -0300

    melhorias

--- listaExc/exercicio1.php
+++ listaExc/exercicio1.php
@@ -1,30 +1,21 @@
 <?php
     $valor = (float) $_POST["valor"];
     $valor_vista = $valor * 0.8;
-
-    $tabela2 = [  
-        1 => [6, number_format(($valor*0.97), 2, '.', ''), number_format((($valor*0.97)/6), 2, '.', '')],
-        2 => [12, number_format(($valor*0.94), 2, '.', ''), number_format((($valor*0.94)/12), 2, '.', '')],
-        3 => [18, number_format(($valor*0.91), 2, '.', ''), number_format((($valor*0.91)/18), 2, '.', '')],
-        4 => [24, number_format(($valor*0.88), 2, '.', ''), number_format((($valor*0.88)/24), 2, '.', '')],
-        5 => [30, number_format(($valor*0.85), 2, '.', ''), number_format((($valor*0.85)/30), 2, '.', '')],
-        6 => [36, number_format(($valor*0.82), 2, '.', ''), number_format((($valor*0.82)/36), 2, '.', '')],
-        7 => [42, number_format(($valor*0.79), 2, '.', ''), number_format((($valor*0.79)/42), 2, '.', '')],
-        8 => [48, number_format(($valor*0.76), 2, '.', ''), number_format((($valor*0.76)/48), 2, '.', '')],
-        9 => [52, number_format(($valor*0.73), 2, '.', ''), number_format((($valor*0.73)/52), 2, '.', '')],
-        10 => [60, number_format(($valor*0.70), 2, '.', ''), number_format((($valor*0.70)/60), 2, '.', '')]
-    ];
+    $descontos = [0.97, 0.94, 0.91, 0.88, 0.85, 0.82, 0.79, 0.76, 0.73, 0.70];
+    $parcelas = [6, 12, 18, 24, 30, 36, 42, 48, 52, 60];
+    $valor_parcela = [0, 0, 0, 0,0, 0, 0, 0, 0,0];
+    $tabela2 = []; 
+    $cabecalho = ["Quantidade de parcelas", "Valor Final", "Valor da parcela"];
+    
+    array_push($tabela2, $cabecalho);
+    for($i = 0; $i < 10; $i++){
+        $descontos[$i] = number_format(($descontos[$i] * $valor), 2, '.', '');
+        $valor_parcela[$i] = number_format(($descontos[$i] / $parcelas[$i]), 2, '.', '');
+        array_push($tabela2, [$parcelas[$i], $descontos[$i], $valor_parcela[$i]]);
+    }
 
     echo "Valor do carro que você escolheu, à vista tem 20% de desconto e fica: R$ $valor_vista. <br><br>";
     $tabela = '<table border="1">';
-    $tabela .='<tbody>';
-    $tabela .='<thead>';
-    $tabela .= '<tr>';
-    $tabela .= '<th>Quantidade de parcelas</th>'; 
-    $tabela .= '<th>Valor final</th>';
-    $tabela .= '<th>Valor da parcela</th>';
-    $tabela .= '</tr>';
-    $tabela .='</thead>';
     $tabela .='<tbody>';
     foreach ($tabela2 as $array => $atributos){
         $tabela .= '<tr>'; 
